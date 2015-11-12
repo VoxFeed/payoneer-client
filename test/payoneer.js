6198b7cdaee0950d081ee6a6cf0faa271fa5a3d1
Luis Argumedo <huicho.argumedo@gmail.com>
2015-11-12 16:36:10 -0600

    Create getAuthRedirectURL and getAPIStatus methods

--- test/payoneer.js
+++ test/payoneer.js
@@ -6,26 +6,26 @@ var Payoneer = require('../lib/payoneer');
 var config = require('./config.json');
 var responses = require('./fixtures/responses.json');
 
-describe('Payoneer Module', () => {
-  describe('Configuration', () => {
-    it('throws when no config is set', () => {
-      expect(() => {
+describe('Payoneer Module', function() {
+  describe('Configuration', function() {
+    it('throws when no config is set', function() {
+      expect(function() {
         /* eslint-disable */
         new Payoneer();
         /* eslint-enable */
       }).to.throw(Error);
     });
 
-    it('throws when a key is missing', () => {
-      expect(() => {
+    it('throws when a key is missing', function() {
+      expect(function() {
         /* eslint-disable */
         new Payoneer({ username: 'yolo' });
         /* eslint-enable */
       }).to.throw(Error);
     });
 
-    it('throws if config keys are provided but empty', () => {
-      expect(() => {
+    it('throws if config keys are provided but empty', function() {
+      expect(function() {
         /* eslint-disable */
         new Payoneer({
           username: '',
@@ -38,43 +38,49 @@ describe('Payoneer Module', () => {
     });
   });
 
-  describe('API Functions', () => {
+  describe('API Functions', function() {
     var payoneer;
-    beforeEach(() => {
+    beforeEach(function() {
       payoneer = new Payoneer(config);
     });
 
-    describe('Misc Functions', () => {
-      it('Echo Function', (done) => {
+    describe('AuthRedirectURL Function', function() {
+      it('Returns an URL', function(done) {
         nock('https://api.sandbox.payoneer.com:443')
           .post('/Payouts/HttpApi/API.aspx')
           .query(true)
-          .reply(200, responses.echo);
+          .reply(200, responses.getToken);
 
-        payoneer.echo((error, data) => {
+        payoneer.getAuthRedirectURL('42', function(error, data) {
           expect(error).to.not.exist;
-          expect(data).to.have.property('Status', '000');
-          expect(data).to.have.property('Description');
+          expect(data).to.be.a('string');
           done();
         });
       });
-
-      it('GetVersion Function', (done) => {
+    });
+    describe('Status Functions', function() {
+      it('GetAPIStatus Function', function(done) {
         nock('https://api.sandbox.payoneer.com:443')
           .post('/Payouts/HttpApi/API.aspx')
           .query(true)
           .reply(200, responses.getVersion);
 
-        payoneer.getVersion((error, data) => {
+        nock('https://api.sandbox.payoneer.com:443')
+          .post('/Payouts/HttpApi/API.aspx')
+          .query(true)
+          .reply(200, responses.echo);
+
+        payoneer.getAPIStatus(function(error, data) {
           expect(error).to.not.exist;
           expect(data).to.have.property('Version');
+          expect(data).to.have.property('Description').and.contains('Ok');
           done();
         });
       });
     });
 
-    describe('Payment Functions', () => {
-      it('RequestPayment Function', (done) => {
+    describe('Payment Functions', function() {
+      it('RequestPayment Function', function(done) {
         var options = {
           paymentID: '42',
           payeeID: '1',
@@ -88,7 +94,7 @@ describe('Payoneer Module', () => {
           .query(true)
           .reply(200, responses.payments.exists);
 
-        payoneer.requestPayment(options, (error, data) => {
+        payoneer.requestPayment(options, function(error, data) {
           expect(error).to.not.exist;
           expect(data).to.have.property('Status', '000');
           done();
